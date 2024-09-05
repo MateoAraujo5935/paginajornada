477740831f846baa5b69e5712c788265aaf0406d
MateoAraujo5935 <159729562+MateoAraujo5935@users.noreply.github.com>
2024-09-05 02:11:42 -0300

    deportes php

---
+++ pages/sports.php
@@ -0,0 +1,199 @@
+<?php
+// Datos deportes
+$sportData = [
+    'football_male' => [
+        'title' => 'Fútbol Masculino',
+        'teamsCB' => [
+            ['name' => 'Figuero', 'class' => '1A', 'flag' => './imagenes/1a.png'],
+            ['name' => 'Figuero B', 'class' => '1A', 'flag' => './imagenes/1a.png'],
+            ['name' => 'La bandeta', 'class' => '1B', 'flag' => './imagenes/1b.png'],
+            ['name' => 'Los sequitos', 'class' => '1C', 'flag' => './imagenes/1c.png'],
+            ['name' => 'Chipi Maffin FC', 'class' => '2A', 'flag' => './imagenes/2a.png'],
+            ['name' => 'Deportivo esta', 'class' => '2B', 'flag' => './imagenes/2b.png'],
+            ['name' => 'La Chispa', 'class' => '2C', 'flag' => './imagenes/2c.png'],
+            ['name' => 'Real Tractor', 'class' => '3A', 'flag' => './imagenes/3a.png'],
+            ['name' => 'La Dinastía', 'class' => '3B', 'flag' => './imagenes/3b.png'],
+            ['name' => 'Zunga FC', 'class' => '3B', 'flag' => './imagenes/3b.png'],
+            ['name' => 'Deportivo Rustico', 'class' => '3C', 'flag' => './imagenes/3c.png'],
+            ['name' => 'Sacapipas FC', 'class' => '3C', 'flag' => './imagenes/3c.png']
+        ],
+        'format_desc_cb' => 'notReady',
+        'fixture_cb' => 'https://challonge.com/es/yi7y5yo4/module',
+
+        'teamsCO' => [
+            ['name' => 'Rumba FC', 'class' => '4A', 'flag' => './imagenes/4a.png'],
+            ['name' => 'La sub 16', 'class' => '4B', 'flag' => './imagenes/4b.png'],
+            ['name' => 'Chori cheto', 'class' => '5A', 'flag' => './imagenes/5a.png'],
+            ['name' => 'Villa Dolmine', 'class' => '5A', 'flag' => './imagenes/5a.png'],
+            ['name' => 'Sector L', 'class' => '5B', 'flag' => './imagenes/5b.png'],
+            ['name' => 'Mopaneta', 'class' => '5C', 'flag' => './imagenes/5c.png'],
+            ['name' => 'La toco y me voy', 'class' => '6B', 'flag' => './imagenes/6b.png'],
+            ['name' => 'Deportivo La Sala', 'class' => '6B', 'flag' => './imagenes/6b.png'],
+            ['name' => 'Santos', 'class' => '6C', 'flag' => './imagenes/6c.png'],
+            ['name' => 'Las sobras', 'class' => '6C', 'flag' => './imagenes/6c.png']
+        ],
+        'format_desc_co' => 'notReady',
+        'fixture_co' => 'notReady'
+    ],
+
+    'football_female' => [
+        'title' => 'Fútbol Femenino',
+        'teamsCB' => [
+            ['name' => 'Las Figuero', 'class' => '1A', 'flag' => './imagenes/1a.png'],
+            ['name' => 'La bandita', 'class' => '1B', 'flag' => './imagenes/1b.png'],
+            ['name' => 'Real maní', 'class' => '1C', 'flag' => './imagenes/1c.png'],
+            ['name' => 'Las danoninas', 'class' => '2A', 'flag' => './imagenes/2a.png'],
+            ['name' => 'Las Leonas FC', 'class' => '2A', 'flag' => './imagenes/2a.png'],
+            ['name' => 'Deportivas estas', 'class' => '2B', 'flag' => './imagenes/2b.png'],
+            ['name' => 'Fuego y Chispa', 'class' => '2C', 'flag' => './imagenes/2c.png'],
+            ['name' => 'Las Renzas', 'class' => '3A', 'flag' => './imagenes/3a.png'],
+            ['name' => 'Al tobillo FC', 'class' => '3B', 'flag' => './imagenes/3b.png'],
+            ['name' => 'Deportivas Rusticas', 'class' => '3C', 'flag' => './imagenes/3c.png'],
+        ],
+        'format_desc_cb' => 'notReady',
+        'fixture_cb' => 'notReady',
+
+        'teamsCO' => [
+            ['name' => 'No sé FC', 'class' => '4A', 'flag' => './imagenes/4a.png'],
+            ['name' => 'Rompe pelotas', 'class' => '4B', 'flag' => './imagenes/4b.png'],
+            ['name' => 'Lado C', 'class' => '4C', 'flag' => './imagenes/4c.png'],
+            ['name' => 'La 16', 'class' => '4C', 'flag' => './imagenes/4c.png'],
+            ['name' => 'La Reserva', 'class' => '5A', 'flag' => './imagenes/5a.png'],
+            ['name' => 'Dreams FC', 'class' => '5B', 'flag' => './imagenes/5b.png'],
+            ['name' => 'Barela Juniors', 'class' => '5C', 'flag' => './imagenes/5c.png'],
+            ['name' => 'Panchas FC', 'class' => '6A', 'flag' => './imagenes/6a.png'],
+            ['name' => 'Combinadas FC', 'class' => '6B', 'flag' => './imagenes/6b.png'],
+            ['name' => 'Canilleras FC', 'class' => '6C', 'flag' => './imagenes/6c.png']
+        ],
+        'format_desc_co' => 'notReady',
+        'fixture_co' => 'notReady'
+    ],
+
+
+    'volleyball' => [
+        'title' => 'Voley',
+        'teamsCB' => [
+            ['name' => 'Las estrellitas', 'class' => '1A', 'flag' => './imagenes/1a.png'],
+            ['name' => 'Playeros', 'class' => '1B', 'flag' => './imagenes/1b.png'],
+            ['name' => 'Milangas', 'class' => '1C', 'flag' => './imagenes/1c.png'],
+            ['name' => 'Manchester Chipi', 'class' => '2A', 'flag' => './imagenes/2a.png'],
+            ['name' => 'Sacachispa', 'class' => '2C', 'flag' => './imagenes/2c.png'],
+            ['name' => 'Ying Yang', 'class' => '3A', 'flag' => './imagenes/3a.png'],
+            ['name' => 'Tobiova', 'class' => '3C', 'flag' => './imagenes/3c.png']
+        ],
+        'format_desc_cb' => 'notReady',
+        'fixture_cb' => 'notReady',
+        'teamsCO' => [
+            ['name' => '¿Y lo malo?', 'class' => '4A', 'flag' => './imagenes/4a.png'],
+            ['name' => 'De Chuecos', 'class' => '4B', 'flag' => './imagenes/4b.png'],
+            ['name' => 'Pituquita Junior', 'class' => '4C', 'flag' => './imagenes/4c.png'],
+            ['name' => 'Gladiadoras', 'class' => '4C', 'flag' => './imagenes/4c.png'],
+            ['name' => 'Los Ganadores', 'class' => '5A', 'flag' => './imagenes/5a.png'],
+            ['name' => 'Barela Juniors', 'class' => '5B', 'flag' => './imagenes/5b.png'],
+            ['name' => '9 de Oro', 'class' => '6A', 'flag' => './imagenes/6a.png'],
+            ['name' => 'Los Meeps', 'class' => '6B', 'flag' => './imagenes/6b.png'],
+            ['name' => 'Los Gurú', 'class' => '6C', 'flag' => './imagenes/6c.png']
+        ],
+        'format_desc_co' => 'notReady',
+        'fixture_co' => 'notReady'
+    ],
+    'handball' => [
+        'title' => 'Handball',
+        'teamsCB' => [
+            ['name' => 'Gladiadorcitos', 'class' => '1A', 'flag' => './imagenes/1a.png'],
+            ['name' => 'Balón mano', 'class' => '1B', 'flag' => './imagenes/1b.png'],
+            ['name' => 'Betwarriors', 'class' => '1C', 'flag' => './imagenes/1c.png'],
+            ['name' => 'La Chineta', 'class' => '2A', 'flag' => './imagenes/2a.png'],
+            ['name' => 'Nouhands', 'class' => '2B', 'flag' => './imagenes/2b.png'],
+            ['name' => 'Las Garras', 'class' => '2C', 'flag' => './imagenes/2c.png'],
+            ['name' => 'Los Cortacorrientes', 'class' => '3B', 'flag' => './imagenes/3b.png']
+        ],
+        'format_desc_cb' => 'notReady',
+        'fixture_cb' => 'notReady',
+        'teamsCO' => [
+            ['name' => 'La Grise', 'class' => '4A', 'flag' => './imagenes/4a.png'],
+            ['name' => 'Los Guerreros', 'class' => '4B', 'flag' => './imagenes/4b.png'],
+            ['name' => 'Nouhands', 'class' => '5A', 'flag' => './imagenes/5a.png'],
+            ['name' => 'Cualquiera', 'class' => '5B', 'flag' => './imagenes/5b.png'],
+            ['name' => 'Bomba Arabic', 'class' => '6B', 'flag' => './imagenes/6b.png']
+        ],
+        'format_desc_co' => 'notReady',
+        'fixture_co' => 'notReady'
+    ],
+    'basketball' => [
+        'title' => 'Básquet',
+        'teamsCB' => [
+            ['name' => 'Lanzadores Galácticos', 'class' => '1B', 'flag' => './imagenes/1b.png'],
+            ['name' => 'Los Picantes', 'class' => '1C', 'flag' => './imagenes/1c.png'],
+            ['name' => 'Los pibes de la mata', 'class' => '2A', 'flag' => './imagenes/2a.png'],
+            ['name' => 'Jorgitos', 'class' => '2B', 'flag' => './imagenes/2b.png'],
+            ['name' => 'La Mata', 'class' => '2C', 'flag' => './imagenes/2c.png'],
+            ['name' => 'Bull Dogs', 'class' => '3B', 'flag' => './imagenes/3b.png'],
+            ['name' => 'Los Truenos', 'class' => '3C', 'flag' => './imagenes/3c.png']
+        ],
+        'format_desc_cb' => 'notReady',
+        'fixture_cb' => 'https://challonge.com/es/4wi3qw7o/module',
+        'teamsCO' => [
+            ['name' => 'Los Qué', 'class' => '4A', 'flag' => './imagenes/4a.png'],
+            ['name' => 'Las bostas Celtas', 'class' => '4B', 'flag' => './imagenes/4b.png'],
+            ['name' => 'El Fútbol', 'class' => '5A', 'flag' => './imagenes/5a.png'],
+            ['name' => 'Choco y Sus Secuases', 'class' => '5B', 'flag' => './imagenes/5b.png'],
+            ['name' => 'La Banda de la Mopa', 'class' => '5C', 'flag' => './imagenes/5c.png'],
+            ['name' => 'Cubículos', 'class' => '6B', 'flag' => './imagenes/6b.png'],
+            ['name' => 'Puro Huevo', 'class' => '6C', 'flag' => './imagenes/6c.png']
+        ],
+        'format_desc_co' => 'notReady',
+        'fixture_co' => 'https://challonge.com/es/406zz2py/module'
+    ]
+
+    // Puedes añadir más deportes aquí...
+];
+
+// Obtener el deporte seleccionado
+$sport = isset($_GET['sport']) && isset($sportData[$_GET['sport']]) ? $_GET['sport'] : '404';
+$sportData = $sportData[$sport];
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles.css">
+    <title><?php echo $sportData['title']; ?></title>
+</head>
+<body>
+    <section class="container">
+    <h2 id="deporte" class="deporte"><?php echo $sportData['title']; ?></h2>
+    <div class="dep_div"></div>
+    <h3>(Ciclo Básico)</h3>
+    <div class="dep_teams">
+        <?php foreach ($sportData['teamsCB'] as $team): ?>
+            <div class="dep_team">
+                <img src="<?php echo $team['flag']; ?>" alt="país">
+                <p class="dep_team_name"><?php echo $team['name']; ?></p>
+                <p class="dep_team_curso">(<?php echo $team['class']; ?>)</p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+    <p class="info_torneo"><?php echo $sportData['format_desc_cb']; ?></p>
+    <iframe src="<?php echo $sportData['fixture_cb']; ?>" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
+    
+    <div class="dep_div"></div>
+
+    <h3>(Ciclo Orientado)</h3>
+    <div class="dep_teams">
+        <?php foreach ($sportData['teamsCO'] as $team): ?>
+            <div class="dep_team">
+                <img src="<?php echo $team['flag']; ?>" alt="país">
+                <p class="dep_team_name"><?php echo $team['name']; ?></p>
+                <p class="dep_team_curso">(<?php echo $team['class']; ?>)</p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <p class="info_torneo"><?php echo $sportData['format_desc_co']; ?></p>
+    <iframe src="<?php echo $sportData['fixture_co']; ?>" width="100%" height="500" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
+    </section>
+</body>
+</html>
