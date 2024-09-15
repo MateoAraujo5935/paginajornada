40d91cc863dc9a709401ab7e063c585eeaec202b
MateoAraujo5935 <159729562+MateoAraujo5935@users.noreply.github.com>
2024-09-15 14:40:50 -0300

    15/9-14:40

---
+++ header.php
@@ -0,0 +1,85 @@
+<header>
+<img src="imagenes/banner.png" alt="">
+<nav class="navbar navbar-expand-lg bg-body-dark"> 
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">INICIO</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Deportes
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="index.php?page=sports&sport=football_male">Fútbol Masculino</a></li>
+                <li><a class="dropdown-item" href="index.php?page=sports&sport=football_female">Fútbol Femenino</a></li>
+                <li><a class="dropdown-item" href="index.php?page=sports&sport=basketball">Básquet</a></li>
+                <li><a class="dropdown-item" href="index.php?page=sports&sport=volleyball">Voley</a></li>
+                <li><a class="dropdown-item" href="index.php?page=sports&sport=handball">Handball</a></li>
+                <div class="dropdown-divider"></div>
+                <h6 class="dropdown-header">Esports</h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="index.php?page=sports&sport=fifa">Fifa</a>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                data-bs-theme="dark"
+                aria-expanded="false"
+              >
+                Arte
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Show de talentos</a></li>
+                <li><a class="dropdown-item" href="#">Dibujo</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Cursos
+              </a>
+              <ul class="dropdown-menu">
+                <h6 class="dropdown-header">Ciclo Orientado</h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="./navbar/cursos/6to.html">6to</a>
+                <a class="dropdown-item" href="#">5to</a>
+                <a class="dropdown-item" href="#">4to</a>
+                <div class="dropdown-divider"></div>
+                <h6 class="dropdown-header">Ciclo básico</h6>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">3ro</a>
+                <a class="dropdown-item" href="#">2do</a>
+                <a class="dropdown-item" href="#">1ro</a>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</header>
--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,34 @@
 <section class="container">
-<h2>JORNADA 2024</h2>
-<p>carrusel
-</p>
-<p>imagenes
-</p>
+<h2 class="display-3">JORNADA 2024</h2>
+
+<p>slider</p>
+
+<!-- normas circulos -->
+
+<div class="container text-center my-5">
+    <h3 class="display-4">NORMAS</h3>
+    <p>(POR FAVOR RESPETAR PARA UNA JORNADA EN PAZ)</p>
+    
+    <div class="row justify-content-center">
+      <!-- Círculo 1 -->
+      <div class="col-12 col-md-4 d-flex justify-content-center">
+        <div class="circle purple-circle">
+          <p>No salir del colegio hasta el final <br>de la jornada escolar por más que <br>ya se haya concluido con las actividades</p>
+        </div>
+      </div>
+      <!-- Círculo 2 -->
+      <div class="col-12 col-md-4 d-flex justify-content-center">
+        <div class="circle blue-circle">
+          <p>DEBE PREVALECER EL RESPETO HACIA <br>PROFESORES Y COMPAÑEROS Y RESPETARSE LAS NORMAS ESCOLARES<br>YA QUE SEGUIMOS ESTANDO EN LA ESCUELA</p>
+        </div>
+      </div>
+      <!-- Círculo 3 -->
+      <div class="col-12 col-md-4 d-flex justify-content-center">
+        <div class="circle red-circle">
+          <p>PRESENTARSE A TODOS LOS PARTIDOS Y<br>ACTIVIDADES CON PUNTUALIDAD PARA QUE<br>LA JORNADA PUEDA SEGUIR CON NORMALIDAD</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </section>
