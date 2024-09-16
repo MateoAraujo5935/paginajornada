bd65feb451eed2206c52dd0d136e04b1d626ef3c
MateoAraujo5935 <159729562+MateoAraujo5935@users.noreply.github.com>
2024-09-16 03:53:51 -0300

    página v0.9

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,52 @@
+
+
 <section class="container">
 <h2 class="display-3">JORNADA 2024</h2>
 
+<!--Countdown-->
+
+<div id="contador">
+        <div><span class="number" id="days">0</span><span>Días</span></div>
+        <div><span class="number" id="hours">00</span><span>Horas</span></div>
+        <div>
+          <span class="number" id="minutes">00</span><span>Minutos</span>
+        </div>
+        <div>
+          <span class="number" id="seconds">00</span><span>Segundos</span>
+        </div>
+      </div>
+
 <p>slider</p>
+<section id="carruselDeportes" style="background-color: red>
+    <div class="carousel">
+      <div class="carousel-item" data-index="0">
+        <img src="./imagenes/carrusel_deportes/1.png" alt="Fútbol masculino" width="495px" height="700px">
+      </div>
+      <div class="carousel-item active" data-index="1">
+        <img src="./imagenes/carrusel_deportes/2.png" alt="Fútbol femenino" width="495px" height="700px">
+      </div>
+      <div class="carousel-item" data-index="2">
+        <img src="./imagenes/carrusel_deportes/3.png" alt="Handball" width="495px" height="700px">
+      </div>
+      <div class="carousel-item" data-index="3">
+        <img src="./imagenes/carrusel_deportes/4.png" alt="Básquet" width="495px" height="700px">
+      </div>
+      <div class="carousel-item" data-index="4">
+        <img src="./imagenes/carrusel_deportes/5.png" alt="Show de talentos" width="495px" height="700px">
+      </div>
+      <div class="carousel-item" data-index="5">
+        <img src="./imagenes/carrusel_deportes/6.png" alt="Dibujo" width="495px" height="700px">
+      </div>
+      <div class="carousel-item" data-index="6">
+        <img src="./imagenes/carrusel_deportes/7.png" alt="Volley" width="495px" height="700px">
+      </div>
+      <div class="carousel-item" data-index="7">
+        <img src="./imagenes/carrusel_deportes/8.png" alt="FIFA" width="495px" height="700px">
+      </div>
+      <button class="carousel-control prev">&lt;</button>
+      <button class="carousel-control next">&gt;</button>
+    </div>
+</section>
 
 <!-- normas circulos -->
 
@@ -30,5 +75,91 @@
       </div>
     </div>
   </div>
+
+<!-- Escudos -->
+  <div class="container">
+  <h3 class="display-4">ESCUDOS</h3>
+
+    <!-- Escudo de 6to año -->
+    <div class="escudo-6to">
+      <a href="pagina6to.html">
+        <img src="./imagenes/escudos/argentina.webp" alt="Escudo de Argentina" class="escudo">
+      </a>
+    </div>
+
+    <!-- Escudos de 1er a 5to año -->
+    <!-- Escudos de 5to año -->
+    <h2>Quinto Año</h2>
+    <div class="escudos-grid">
+      <a href="pagina1ero_1.html">
+        <img src="./imagenes/escudos/rusia.png" alt="5to A" class="escudo">
+      </a>
+      <a href="pagina1ero_2.html">
+        <img src="./imagenes/escudos/chile.png" alt="5to B" class="escudo">
+      </a>
+      <a href="pagina1ero_3.html">
+        <img src="./imagenes/escudos/grecia.png" alt="5to C" class="escudo">
+      </a>
+    </div>
+
+    <!-- Escudos de 4to año -->
+    <h2>Cuarto Año</h2>
+    <div class="escudos-grid">
+      <a href="pagina2do_1.html">
+        <img src="./imagenes/escudos/mexico.png" alt="4to A" class="escudo">
+      </a>
+      <a href="pagina2do_2.html">
+        <img src="./imagenes/escudos/holanda.png" alt="4to B" class="escudo">
+      </a>
+      <a href="pagina2do_3.html">
+        <img src="./imagenes/escudos/japon.png" alt="4to C" class="escudo">
+      </a>
+    </div>
+
+    <!-- Escudos de 3er año -->
+    <h2>Tercer Año</h2>
+    <div class="escudos-grid">
+      <a href="pagina1ero_1.html">
+        <img src="./imagenes/escudos/francia.png" alt="3ro A" class="escudo">
+      </a>
+      <a href="pagina1ero_2.html">
+        <img src="./imagenes/escudos/españa.png" alt="3ro B" class="escudo">
+      </a>
+      <a href="pagina1ero_3.html">
+        <img src="./imagenes/escudos/croacia.png" alt="3ro C" class="escudo">
+      </a>
+    </div>
+
+    <!-- Escudos de 2do año -->
+    <h2>Segundo Año</h2>
+    <div class="escudos-grid">
+      <a href="pagina1ero_1.html">
+        <img src="./imagenes/escudos/brasil.png" alt="2ndo A" class="escudo">
+      </a>
+      <a href="pagina1ero_2.html">
+        <img src="./imagenes/escudos/italia.png" alt="2ndo B" class="escudo">
+      </a>
+      <a href="pagina1ero_3.html">
+        <img src="./imagenes/escudos/inglaterra.webp" alt="2ndo C" class="escudo">
+      </a>
+    </div>
+
+    <!-- Escudos de 1er año -->
+    <h2>Primer Año</h2>
+    <div class="escudos-grid">
+      <a href="pagina1ero_1.html">
+        <img src="./imagenes/escudos/australia.png" alt="1ero A" class="escudo">
+      </a>
+      <a href="pagina1ero_2.html">
+        <img src="./imagenes/escudos/alemania.png" alt="1ero B" class="escudo">
+      </a>
+      <a href="pagina1ero_3.html">
+        <img src="./imagenes/escudos/uruguay.png" alt="1ero C" class="escudo">
+      </a>
+    </div>
+
+  </div>
+<!--SCRIPTS-->
+<script src="../js/countdown.js"></script>
 
 </section>
--- pages/sports.php
+++ pages/sports.php
@@ -66,7 +66,7 @@ $sportData = [
             ['name' => 'Canilleras FC', 'class' => '6C', 'flag' => './imagenes/flags/6.webp']
         ],
         'format_desc_co' => 'notReady',
-        'fixture_co' => 'notReady'
+        'fixture_co' => 'https://challonge.com/es/vcxei3yc/module'
     ],
 
     'volleyball' => [
@@ -81,7 +81,7 @@ $sportData = [
             ['name' => 'Tobiova', 'class' => '3C', 'flag' => './imagenes/flags/3c.webp']
         ],
         'format_desc_cb' => 'notReady',
-        'fixture_cb' => 'notReady',
+        'fixture_cb' => 'https://challonge.com/es/q57cwf9t/module',
         'teamsCO' => [
             ['name' => '¿Y lo malo?', 'class' => '4A', 'flag' => './imagenes/flags/4a.webp'],
             ['name' => 'De Chuecos', 'class' => '4B', 'flag' => './imagenes/flags/4b.webp'],
@@ -93,7 +93,7 @@ $sportData = [
             ['name' => 'Los Gurú', 'class' => '6C', 'flag' => './imagenes/flags/6.webp']
         ],
         'format_desc_co' => 'notReady',
-        'fixture_co' => 'notReady'
+        'fixture_co' => 'https://challonge.com/es/6zkqbnts/module'
     ],
 
     'handball' => [
@@ -108,7 +108,7 @@ $sportData = [
             ['name' => 'Los Cortacorrientes', 'class' => '3B', 'flag' => './imagenes/flags/3b.webp']
         ],
         'format_desc_cb' => 'notReady',
-        'fixture_cb' => 'notReady',
+        'fixture_cb' => 'https://challonge.com/es/8go9sdps/module',
         'teamsCO' => [
             ['name' => 'La Grise', 'class' => '4A', 'flag' => './imagenes/flags/4a.webp'],
             ['name' => 'Los Guerreros', 'class' => '4B', 'flag' => './imagenes/flags/4b.webp'],
@@ -117,7 +117,7 @@ $sportData = [
             ['name' => 'Bomba Arabic', 'class' => '6B', 'flag' => './imagenes/flags/6.webp']
         ],
         'format_desc_co' => 'notReady',
-        'fixture_co' => 'notReady'
+        'fixture_co' => 'https://challonge.com/es/ig68so0s/module'
     ],
 
     'basketball' => [
@@ -144,7 +144,68 @@ $sportData = [
         ],
         'format_desc_co' => 'notReady',
         'fixture_co' => 'https://challonge.com/es/406zz2py/module'
-    ]
+    ],
+
+    'fifa' => [
+        'title' => 'FIFA',
+        'teamsCB' => [
+            ['name' => 'Los Pollitos FC', 'class' => '1A', 'flag' => './imagenes/flags/1a.webp'],
+            ['name' => 'La Banda FC', 'class' => '1A', 'flag' => './imagenes/flags/1a.webp'],
+            ['name' => 'Los Galácticos FC', 'class' => '1A', 'flag' => './imagenes/flags/1a.webp'],
+            ['name' => 'Sanguchitos FC', 'class' => '1A', 'flag' => './imagenes/flags/1a.webp'],
+            ['name' => 'Nafta Con Speed', 'class' => '1A', 'flag' => './imagenes/flags/1a.webp'],
+            ['name' => 'Los De La Noche', 'class' => '1B', 'flag' => './imagenes/flags/1b.webp'],
+            ['name' => 'Maripositas Alocaditas', 'class' => '1B', 'flag' => './imagenes/flags/1b.webp'],
+            ['name' => 'Los Baltas', 'class' => '1B', 'flag' => './imagenes/flags/1b.webp'],
+            ['name' => 'Puercos FC', 'class' => '1B', 'flag' => './imagenes/flags/1b.webp'],
+            ['name' => '1/11/14', 'class' => '1C', 'flag' => './imagenes/flags/1c.webp'],
+            ['name' => 'Manchester Pritty', 'class' => '1C', 'flag' => './imagenes/flags/1c.webp'],
+            ['name' => 'Belplate', 'class' => '1C', 'flag' => './imagenes/flags/1c.webp'],
+            ['name' => 'Rústicos FC', 'class' => '1C', 'flag' => './imagenes/flags/1c.webp'],
+            ['name' => 'La Virzineta', 'class' => '2A', 'flag' => './imagenes/flags/2a.webp'],
+            ['name' => 'La Dupla Galáctica', 'class' => '2A', 'flag' => './imagenes/flags/2a.webp'],
+            ['name' => 'AC Frío', 'class' => '2A', 'flag' => './imagenes/flags/2a.webp'],
+            ['name' => 'Panchoneta', 'class' => '2B', 'flag' => './imagenes/flags/2b.webp'],
+            ['name' => 'Barcecola', 'class' => '2B', 'flag' => './imagenes/flags/2b.webp'],
+            ['name' => 'Cloaca FC', 'class' => '2B', 'flag' => './imagenes/flags/2b.webp'],
+            ['name' => 'Nottingham Miedo', 'class' => '2B', 'flag' => './imagenes/flags/2b.webp'],
+            ['name' => 'Atlético Pururú', 'class' => '2C', 'flag' => './imagenes/flags/2c.webp'],
+            ['name' => 'La Morcineta', 'class' => '2C', 'flag' => './imagenes/flags/2c.webp'],
+            ['name' => 'Los Matadores', 'class' => '3A', 'flag' => './imagenes/flags/3a.webp'],
+            ['name' => 'Los Piratas', 'class' => '3A', 'flag' => './imagenes/flags/3a.webp'],
+            ['name' => 'Camisetas Nani', 'class' => '3B', 'flag' => './imagenes/flags/3b.webp'],
+            ['name' => 'La Chancletilla', 'class' => '3B', 'flag' => './imagenes/flags/3b.webp'],
+            ['name' => 'Pesuti', 'class' => '3B', 'flag' => './imagenes/flags/3b.webp'],
+            ['name' => 'La Banda Del Babolineo', 'class' => '3B', 'flag' => './imagenes/flags/3b.webp'],
+            ['name' => 'La Mafia A Colocar', 'class' => '3B', 'flag' => './imagenes/flags/3b.webp'],
+            ['name' => 'La Chispa', 'class' => '3B', 'flag' => './imagenes/flags/3b.webp'],
+            ['name' => 'Tengo Fé', 'class' => '3C', 'flag' => './imagenes/flags/3c.webp'],
+            ['name' => 'Cokismo FC', 'class' => '3C', 'flag' => './imagenes/flags/3c.webp']
+            
+        ],
+        'format_desc_cb' => 'notReady',
+        'fixture_cb' => 'https://challonge.com/es/gn9mk8b0/module',
+        'teamsCO' => [
+            ['name' => 'Sacachispas', 'class' => '6B', 'flag' => './imagenes/flags/6.webp'],
+            ['name' => 'Pisala Pomelo', 'class' => '4A', 'flag' => './imagenes/flags/4a.webp'],
+            ['name' => 'Pase Al Medio', 'class' => '6A', 'flag' => './imagenes/flags/6.webp'],
+            ['name' => 'Los Discipulos De Gastón', 'class' => '5A', 'flag' => './imagenes/flags/5a.webp'],
+            ['name' => 'Muletas FC', 'class' => '5A', 'flag' => './imagenes/flags/5a.webp'],
+            ['name' => 'GAGAstón', 'class' => '6B', 'flag' => './imagenes/flags/6.webp'],
+            ['name' => 'TikiTaka', 'class' => '6A', 'flag' => './imagenes/flags/6.webp'],
+            ['name' => 'Convivencias FC', 'class' => '5A', 'flag' => './imagenes/flags/5a.webp'],
+            ['name' => 'Mostoles Dux', 'class' => '5B', 'flag' => './imagenes/flags/5b.webp'],
+            ['name' => 'Gastón Pittaro Y Sus Redonditos De Ricota', 'class' => '5A', 'flag' => './imagenes/flags/5a.webp'],
+            ['name' => 'Mari FC', 'class' => '4C', 'flag' => './imagenes/flags/4c.webp'],
+            ['name' => 'La Banda De La Luján', 'class' => '6C', 'flag' => './imagenes/flags/6.webp'],
+            ['name' => 'La Mafia De Pastoral', 'class' => '5A', 'flag' => './imagenes/flags/5a.webp'],
+            ['name' => 'Sirux FC', 'class' => '4B', 'flag' => './imagenes/flags/4b.webp'],
+            ['name' => 'La Gastoneta', 'class' => '5A', 'flag' => './imagenes/flags/5a.webp'],
+            ['name' => 'Max Win', 'class' => '6C', 'flag' => './imagenes/flags/6.webp']
+        ],
+        'format_desc_co' => 'notReady',
+        'fixture_co' => 'https://challonge.com/es/obxc9r9/module'
+    ],
 ];
 
 // Obtener el deporte seleccionado
